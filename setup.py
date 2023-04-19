5d0909ffb4a0df0a3877fc9180d0333bfa0220dd
Sneha Mulrajani <sneha.mulrajani@accelextech.com>
2023-04-19 15:47:17 +0100

    add req

--- setup.py
+++ setup.py
@@ -7,11 +7,6 @@ with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
 
-with open("requirements-dev.txt") as f:
-    tests_require = [
-        line for line in f.read().splitlines() if line != "-r requirements.txt"
-    ]
-
 setup(
       name="acx-twirpy",
       version="0.1.dev0",
@@ -22,7 +17,6 @@ setup(
       packages=find_packages(where="src"),
       package_dir={"": "src"},
       install_requires=install_requires,
-      test_requires=tests_require,
       python_requires=">=3.8",
       zip_safe=True
       )
